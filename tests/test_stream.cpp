da0fa60cc1c612766bb579d461b7f07380a1f950
Vladislav Volkov <wwwvladislav@gmail.com>
2019-05-29 09:59:31 +0300

    formatting

--- tests/test_stream.cpp
+++ tests/test_stream.cpp
@@ -7,36 +7,36 @@
 using namespace plusar;
 using namespace std;
 
-TEST_CASE("Stream creation by initializer list", "[stream]" ) {
+TEST_CASE("Stream creation by initializer list", "[stream]") {
     REQUIRE(make_stream({ 42 })
                 .collect() == 42);
 }
 
-TEST_CASE("Stream creation by functor", "[stream]" ) {
+TEST_CASE("Stream creation by functor", "[stream]") {
     REQUIRE(make_stream([]() { return make_optional(42); })
                 .take(1)
                 .collect() == 42);
 }
 
-TEST_CASE("Filter function", "[stream]" ) {
+TEST_CASE("Filter function", "[stream]") {
     REQUIRE(make_stream({ 1, 2, 3 })
                 .filter([](int t) { return t % 2 == 0; })
                 .collect() == 2);
 }
 
-TEST_CASE("Map function", "[stream]" ) {
+TEST_CASE("Map function", "[stream]") {
     REQUIRE(make_stream({ 21 })
                 .map([](int t) { return t * 2; })
                 .collect() == 42);
 }
 
-TEST_CASE("Reduce function", "[stream]" ) {
+TEST_CASE("Reduce function", "[stream]") {
     REQUIRE(make_stream({ 20, 22 })
                 .reduce(0, std::plus<>())
                 .collect() == 42);
 }
 
-TEST_CASE("Flatten stream 1", "[stream]" ) {
+TEST_CASE("Flatten stream 1", "[stream]") {
     auto s1 = make_stream({ 1, 2, 3 });
     auto s2 = make_stream({ 4, 5, 6 });
     auto s3 = make_stream({ 7, 8, 9 });
@@ -50,11 +50,11 @@ TEST_CASE("Flatten stream 1", "[stream]" ) {
     REQUIRE_THROWS(ss.collect());
 }
 
-TEST_CASE("Flatten stream 2", "[stream]" ) {
+TEST_CASE("Flatten stream 2", "[stream]") {
     int n = 0;
     auto ss = make_stream([&n]()
     {
-        return std::make_optional(make_stream([&n] () mutable -> std::optional<int>
+        return std::make_optional(make_stream([&n] () mutable
         {
             return std::make_optional(++n);
         })
@@ -69,14 +69,14 @@ TEST_CASE("Flatten stream 2", "[stream]" ) {
     REQUIRE_THROWS(ss.collect());
 }
 
-TEST_CASE("Take N elements", "[stream]" ) {
+TEST_CASE("Take N elements", "[stream]") {
     REQUIRE(make_stream([]() { return make_optional(5); })
                 .take(3)
                 .reduce(0, std::plus<>())
                 .collect() == 15);
 }
 
-TEST_CASE("Skip N elements", "[stream]" ) {
+TEST_CASE("Skip N elements", "[stream]") {
     REQUIRE(make_stream([n = 0]() mutable { return make_optional(n++); })
                 .skip(3)
                 .take(3)
@@ -90,7 +90,7 @@ TEST_CASE("Skip N elements", "[stream]" ) {
                 .collect() == 12);
 }
 
-TEST_CASE("Zip streams", "[stream]" ) {
+TEST_CASE("Zip streams", "[stream]") {
     auto s1 = make_stream({ 1, 2, 3 });
     auto s2 = make_stream({ -1, -2, -3, -4 });
     auto ss = s1.zip(std::move(s2), std::plus<>());
@@ -101,7 +101,7 @@ TEST_CASE("Zip streams", "[stream]" ) {
     REQUIRE_THROWS(ss.collect());
 }
 
-TEST_CASE("Slice stream", "[stream]" ) {
+TEST_CASE("Slice stream", "[stream]") {
     size_t step = 2;
 
     for(size_t start = 0; start < 5; ++start)
@@ -117,7 +117,7 @@ TEST_CASE("Slice stream", "[stream]" ) {
     }
 }
 
-TEST_CASE("Slice stream to end", "[stream]" ) {
+TEST_CASE("Slice stream to end", "[stream]") {
     size_t step = 2;
 
     for(size_t start = 0; start < 5; ++start)
@@ -130,7 +130,7 @@ TEST_CASE("Slice stream to end", "[stream]" ) {
     }
 }
 
-TEST_CASE("Collect items from stream", "[stream]" ) {
+TEST_CASE("Collect items from stream", "[stream]") {
     std::vector<int> v;
 
     make_stream([n = 0]() mutable { return make_optional(n++); })
@@ -142,7 +142,7 @@ TEST_CASE("Collect items from stream", "[stream]" ) {
         REQUIRE(n == m++);
 }
 
-TEST_CASE("Read next item from stream", "[stream]" ) {
+TEST_CASE("Read next item from stream", "[stream]") {
 
     auto s = make_stream([n = 0]() mutable { return make_optional(n++); });
     REQUIRE(s.next() == 0);
